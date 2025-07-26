81238191d48ea40aee1fc9ecdf5f0cb20f0b06c1
readf0x <davis.a.forsythe@gmail.com>
2025-07-25 20:45:14 -0500

    allow multiple files, automatically run `go run`
    downside is requires go compiler at runtime, but that's kinda a non
    issue tbh

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"io"
+	"log"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func w(str string, o *os.File) {
@@ -13,15 +16,21 @@ func wb(b byte, o *os.File) {
 }
 
 func main() {
-	// name := os.Args[1]
-	name := "index.html.tet"
-	f, _ := os.Open(name)
-	defer f.Close()
-	out, _ := os.Create("build.go")
-	defer out.Close()
-	b, _ := io.ReadAll(f)
-	content := string(b)
-	w(`//go:build ignore
+	if len(os.Args) == 1 {
+		println("No files")
+		os.Exit(2)
+	} else {
+		for _, A := range os.Args[1:] {
+			if strings.HasSuffix(A, ".tet") {
+				outname := A[:len(A)-4]
+				println(outname)
+				f, _ := os.Open(A)
+				defer f.Close()
+				fname := "build-" + strings.ReplaceAll(outname, "/", "_") + ".go"
+				out, _ := os.Create(fname)
+				b, _ := io.ReadAll(f)
+				content := string(b)
+				w(`//go:build ignore
 package main
 
 import (
@@ -29,7 +38,7 @@ import (
 	"fmt"
 )
 
-var out, _ = os.Create("`+name[:len(name)-4]+`")
+var out, _ = os.Create("`+outname+`")
 
 func w(str string) {
 	out.Write([]byte(fmt.Sprint(str)))
@@ -38,34 +47,44 @@ func w(str string) {
 func main() {
 	defer out.Close()
 `, out)
-	w("w(`", out)
-	lft := false
-	var prefix, suffix string
-	for i := 0; i < len(content); i++ {
-		s := content[i:]
-		if len(s) > 1 {
-			switch s[:2] {
-			case "<|":
-				w("`);\n", out)
-				if s[2] == ':' {
-					lft = true
-					prefix, suffix = pft(s[3])
-					w(prefix, out)
-					i+=2
+				w("w(`", out)
+				lft := false
+				var prefix, suffix string
+				for i := 0; i < len(content); i++ {
+					s := content[i:]
+					if len(s) > 1 {
+						switch s[:2] {
+						case "<|":
+							w("`);\n", out)
+							if s[2] == ':' {
+								lft = true
+								prefix, suffix = pft(s[3])
+								w(prefix, out)
+								i += 2
+							}
+							i += 2
+						case "|>":
+							if lft {
+								w(suffix, out)
+								lft = false
+							}
+							w(";w(`", out)
+							i += 2
+						}
+					}
+					wb(content[i], out)
 				}
-				i+=2
-			case "|>":
-				if lft {
-					w(suffix, out)
-					lft = false
+				w("`);\n}\n", out)
+
+				cmd := exec.Command("go", "run", fname)
+				if err := cmd.Run(); err != nil {
+					log.Fatal(err)
 				}
-				w(";w(`", out)
-				i+=2
+				out.Close()
+				os.Remove(fname)
 			}
-			wb(content[i], out)
 		}
 	}
-	w("`);\n}\n", out)
 }
 
 func pft(char byte) (string, string) {
@@ -75,4 +94,3 @@ func pft(char byte) (string, string) {
 	}
 	return "", ""
 }
-
